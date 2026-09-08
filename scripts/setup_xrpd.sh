b52a44e32f19f97f551ec110407a91deb81eb81c
NicoGiba09 <lorenzoarrudagibala@gmail.com>
2026-09-08 18:46:32 -0300

    Update setup_xrpd.sh

--- scripts/setup_xrpd.sh
+++ scripts/setup_xrpd.sh
@@ -1,23 +1,4 @@
-#@title **Create User**
-import os
-username = "Kakashi" #@param {type:"string"}
-password = "@Kakashi9897@" #@param {type:"string"}
 
-print("Creating User and Setting it up")
-
-# Creat user
-os.system(f"useradd -m {username}")
-
-# Add user to sudo group
-os.system(f"adduser {username} sudo")
-
-# Set password user to 'root'
-os.system(f"echo '{username}:{password}' | sudo chpasswd")
-
-# Change default shell from sh to bash
-os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
-
-print("User Created and Configured")
 
 #!/usr/bin/env bash
 # setup_xrdp.sh - Instala KDE Plasma + XRDP en Google Colab.
